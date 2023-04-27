179403a1a15090b06e67ebe542a99760ebb32d1a
AbdallaM2 <119325558+AbdallaM2@users.noreply.github.com>
2023-04-27 17:47:35 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -56,10 +56,16 @@ async def give_filter(client, message):
                         await auto_filter(client, message)
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
-async def pv_filter(client, message):
-    kd = await global_filters(client, message)
-    if kd == False:
-        await auto_filter(client, message)
+async def auto_pm_fill(b, m):
+    if PMFILTER.strip().lower() in ["true", "yes", "1", "enable", "y"]:       
+        if G_FILTER:
+            kd = await global_filters(b, m)
+            if kd == False:
+                await pm_AutoFilter(b, m)
+        else:      
+            await pm_AutoFilter(b, m)
+    elif PMFILTER.strip().lower() in ["false", "no", "0", "disable", "n"]:
+        return 
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):

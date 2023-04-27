0e5c01b67910d77a2c667be22c0077f842243790
AbdallaM2 <119325558+AbdallaM2@users.noreply.github.com>
2023-04-27 18:35:34 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -493,10 +493,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
     if query.data.startswith("file"):
-        ident, req, file_id = query.data.split("#")
-        if BUTTON_LOCK.strip().lower() in ["true", "yes", "1", "enable", "y"]:
-            if int(req) not in [query.from_user.id, 0]:
-                return await query.answer(BUTTON_LOCK_TEXT.format(query=query.from_user.first_name), show_alert=True)             
+        ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
         if not files_:
             return await query.answer('No such file exist.')
